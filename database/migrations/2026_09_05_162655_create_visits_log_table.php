30c9be44cca29ee9c9d485f650e6a32a7e94a088
saria <sarianaja2114@gmail.com>
2026-09-05 19:51:04 +0300

    migrations v1

---
+++ database/migrations/2026_09_05_162655_create_visits_log_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('visits_log', function (Blueprint $table) {
+            $table->id('visit_id');
+            $table->integer('visitor_id');
+            $table->foreignId('staff_id')->constrained('staff', 'staff_id')->onDelete('cascade');
+            
+            $table->dateTime('check_in_time');
+            $table->dateTime('check_out_time')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('visits_log');
+    }
+};
